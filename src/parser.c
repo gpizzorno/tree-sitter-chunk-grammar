3b46483302e0c861305022abe2ff6a3b96cca59e
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2021-07-25 13:53:41 -0400

    changed tree-sitter version
    lowered tree-sitter version to 0.18.3 to support Atom

--- src/parser.c
+++ src/parser.c
@@ -5,7 +5,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-#define LANGUAGE_VERSION 13
+#define LANGUAGE_VERSION 12
 #define STATE_COUNT 131
 #define LARGE_STATE_COUNT 2
 #define SYMBOL_COUNT 62
@@ -14,7 +14,6 @@
 #define EXTERNAL_TOKEN_COUNT 0
 #define FIELD_COUNT 0
 #define MAX_ALIAS_SEQUENCE_LENGTH 5
-#define PRODUCTION_ID_COUNT 5
 
 enum {
   sym_iob_tag = 1,
@@ -468,7 +467,7 @@ static const TSSymbolMetadata ts_symbol_metadata[] = {
   },
 };
 
-static TSSymbol ts_alias_sequences[PRODUCTION_ID_COUNT][MAX_ALIAS_SEQUENCE_LENGTH] = {
+static TSSymbol ts_alias_sequences[5][MAX_ALIAS_SEQUENCE_LENGTH] = {
   [0] = {0},
   [1] = {
     [0] = sym_class_character,
@@ -3998,22 +3997,21 @@ extern const TSLanguage *tree_sitter_chunkgrammar(void) {
     .alias_count = ALIAS_COUNT,
     .token_count = TOKEN_COUNT,
     .external_token_count = EXTERNAL_TOKEN_COUNT,
-    .state_count = STATE_COUNT,
-    .large_state_count = LARGE_STATE_COUNT,
-    .production_id_count = PRODUCTION_ID_COUNT,
-    .field_count = FIELD_COUNT,
-    .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
-    .parse_table = (const uint16_t *)ts_parse_table,
-    .small_parse_table = (const uint16_t *)ts_small_parse_table,
-    .small_parse_table_map = (const uint32_t *)ts_small_parse_table_map,
-    .parse_actions = ts_parse_actions,
     .symbol_names = ts_symbol_names,
     .symbol_metadata = ts_symbol_metadata,
+    .parse_table = (const uint16_t *)ts_parse_table,
+    .parse_actions = ts_parse_actions,
+    .lex_modes = ts_lex_modes,
+    .alias_sequences = (const TSSymbol *)ts_alias_sequences,
+    .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
+    .lex_fn = ts_lex,
+    .field_count = FIELD_COUNT,
+    .large_state_count = LARGE_STATE_COUNT,
+    .small_parse_table = (const uint16_t *)ts_small_parse_table,
+    .small_parse_table_map = (const uint32_t *)ts_small_parse_table_map,
     .public_symbol_map = ts_symbol_map,
     .alias_map = ts_non_terminal_alias_map,
-    .alias_sequences = (const TSSymbol *)ts_alias_sequences,
-    .lex_modes = ts_lex_modes,
-    .lex_fn = ts_lex,
+    .state_count = STATE_COUNT,
   };
   return &language;
 }
